09b6e62d27730433c484f6e56a08b04e9e56f670
lassepitknen <lassepitknen@gmail.com>
2019-07-29 20:19:03 +0300

    test(*): add DbContextFacatory & initial tests

---
+++ LohiWeb.Tests/DbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using LohiWeb.Data;
+using LohiWeb.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace LohiWeb.Tests
+{
+    public class DbContextFactory
+    {
+        public ILohiDbContext BuildDbContext()
+        {
+            var options = new DbContextOptionsBuilder<LohiDbContext>()
+                .UseInMemoryDatabase(databaseName: "Database")
+                .Options;
+
+            var dbContext = new LohiDbContext(options);
+            dbContext.WaterLevelLocation.Add(new WaterLevelLocation { Id = 1, Name = "Pello", WaterLevels = new List<WaterLevel>() });
+            dbContext.SaveChanges();
+
+            return dbContext;
+        }
+    }
+}
---
+++ LohiWeb.Tests/Repositories/WaterLevelLocationRespositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using LohiWeb.Data.Repositories;
+using System.Threading.Tasks;
+using LohiWeb.Data;
+using System.Linq;
+
+namespace LohiWeb.Tests.Repositories
+{
+    public class WaterLevelLocationRespositoryTests
+    {
+        private DbContextFactory _dbContextFactory;
+
+        public WaterLevelLocationRespositoryTests()
+        {
+            _dbContextFactory = new DbContextFactory();
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnData()
+        {
+            //Arrange
+            var _dbContext = _dbContextFactory.BuildDbContext();
+            var sut = new WaterLevelLocationRepository(_dbContext);
+
+            var expected = _dbContext.WaterLevelLocation;
+
+            //Act
+            var actual = await sut.GetAll();
+ 
+            //Assert
+            actual.Should().Equal(expected);
+        }
+    }
+}
