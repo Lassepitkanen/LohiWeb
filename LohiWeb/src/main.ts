e1fa39adaee17bfb66738a2fbb888f59e788e4b5
lassepitknen <lassepitknen@gmail.com>
2019-08-05 00:24:16 +0300

    chore(package*): add aurelia-store & nprogress

--- LohiWeb/src/main.ts
+++ LohiWeb/src/main.ts
@@ -1,6 +1,8 @@
-﻿import { Aurelia, PLATFORM } from "aurelia-framework";
+﻿import { Aurelia, PLATFORM } from 'aurelia-framework';
+import { initialState } from './shared/state/state';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'nprogress/nprogress.css';
 import './styles/styles.css';
 
 declare const __DEBUG__: boolean; // This is defined by Webpack build
@@ -11,6 +13,7 @@ export function configure(aurelia: Aurelia) {
   if (__DEBUG__) {
     aurelia.use.developmentLogging();
   }
+  aurelia.use.plugin(PLATFORM.moduleName('aurelia-store'), { initialState });
 
   aurelia.start()
     .then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
