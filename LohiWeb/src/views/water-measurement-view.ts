348e23ffa0c6f0ed6e031e4c3b95ca7bc852b683
lassepitknen <lassepitknen@gmail.com>
2019-08-17 19:58:10 +0300

    feat(water-measurement-view): add send button & location-select

--- LohiWeb/src/views/water-measurement-view.ts
+++ LohiWeb/src/views/water-measurement-view.ts
@@ -1,4 +1,13 @@
+import { bindable } from 'aurelia-framework';
 
 export class WaterMeasurementView {
+  @bindable
+  public selectedLocationId: number|undefined;
+  @bindable
+  public measurementData: Object[];
 
+  saveToDb() {
+    console.log(this.selectedLocationId);
+    console.log(this.measurementData);
+  }
 }
