e4b5b25e4d41a54debaafba39a7c4ea9799bfe22
lassepitknen <lassepitknen@gmail.com>
2019-08-17 15:03:10 +0300

    perf(file-input): improve csv parsing speed

--- LohiWeb/src/components/file-input.ts
+++ LohiWeb/src/components/file-input.ts
@@ -1,13 +1,11 @@
 import { bindable } from 'aurelia-framework';
 
-
 export class FileInput {
   @bindable
   public measurementData: Object[] = [];
 
   private data: Object[] = [];
   private fileList: FileList|null = null;
-  private names: string[] = ['unixTime', 'depth', 'heatmap', 'lat', 'lng', 'alt', 'speed', 'heading', 'latError', 'lngError', 'altError', 'airTemp', 'waterTemp'];
 
   private async onSelectFile(e: Event) {
     if (this.fileList) {
@@ -28,19 +26,14 @@ export class FileInput {
         let result = e.target.result;
         result = result.replace(/[\r\n]+/g, ' ').split(' ');
         
-        result = result.map((row: string) => {
-          let obj = Object.assign({}, row.split(','));
-          this.names.forEach((name, index) => {
-            const oldProp = Object.getOwnPropertyDescriptor(obj, index) as PropertyDescriptor;
-            if (oldProp) {
-              Object.defineProperty(obj, name, oldProp);
-              delete obj[index];
-            }
-          });
-          return obj;
-        });
-        
-        resolve(result);
+        const len = result.length;
+        const waterMeasurements = Array(len)
+        for (let i = 0; i < len; i++) {
+          const values = result[i].split(',');
+          waterMeasurements[i] = new WaterMeasurement(values);
+        }
+
+        resolve(waterMeasurements);
       }
       fileReader.onerror = () => {
         reject([]);
@@ -49,5 +42,35 @@ export class FileInput {
   
       fileReader.readAsText(file);
     });
+  }
+}
+
+class WaterMeasurement {
+  private depth: [Number, String];
+  private heatmap: [Number, String];
+  private lat: [Number, String];
+  private lng: [Number, String];
+  private alt: [Number, String];
+  private speed: [Number, String];
+  private heading: [Number, String];
+  private latError: [Number, String];
+  private lngError: [Number, String];
+  private altError: [Number, String];
+  private airTemp: [Number, String];
+  private waterTemp: [Number, String]
+
+  constructor(values: Array<[Number, String]>) {
+    this.depth = values[0];
+    this.heatmap = values[1];
+    this.lat = values[2];
+    this.lng = values[3];
+    this.alt = values[4];
+    this.speed = values[5];
+    this.heading = values[6];
+    this.latError = values[7];
+    this.lngError = values[8];
+    this.altError = values[9];
+    this.airTemp = values[10];
+    this.waterTemp = values[11];
   }
 }
