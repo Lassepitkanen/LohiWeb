84fb03a06ec0cbb51d6ebe8dc64bc600557bc0d0
lassepitknen <lassepitknen@gmail.com>
2019-08-17 19:53:00 +0300

    feat(state): add actions & waterLevelLocations to state

--- LohiWeb/src/shared/state/state.ts
+++ LohiWeb/src/shared/state/state.ts
@@ -1,7 +1,32 @@
+import { IWaterLevelLocation } from './../models/water-level-location';
+
 export interface State {
-  frameworks: string[];
+  waterLevelLocations: IWaterLevelLocation[];
 }
 
 export const initialState: State = {
-  frameworks: ['Aurelia', 'React', 'Angular']
+  waterLevelLocations: [],
 };
+
+export const setWaterLevelLocationsAction = (state: State, waterLevelLocations: IWaterLevelLocation[]) => {
+  const newState = Object.assign({}, state);
+  newState.waterLevelLocations = waterLevelLocations;
+
+  return newState;
+}
+
+export const addWaterLevelLocationAction = (state: State, waterLevelLocation: IWaterLevelLocation) => {
+  const newState = Object.assign({}, state);
+  newState.waterLevelLocations = [...newState.waterLevelLocations, waterLevelLocation];
+
+  return newState;
+}
+
+export const deleteWaterLevelLocationAction = (state: State, index: number) => {
+  const newState = Object.assign({}, state);
+  const locations = [...newState.waterLevelLocations];
+  locations.splice(index, 1);
+  newState.waterLevelLocations = locations;
+
+  return newState;
+}
